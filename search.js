a58342961acf8f64327a00e3e755b0e37ebd28ed
Michael Harris <mhguitar1997@gmail.com>
2018-01-11 12:45:39 -0800

    Gallery Update
    Tried to fix search

--- search.js
+++ search.js
@@ -1,4 +1,4 @@
-const search = document.querySelector('#searchBox');
+/*const search = document.querySelector('#searchBox');
 const photos = document.querySelectorAll('.photo');
 const img = document.querySelectorAll('img');
 const alt = document.querySelectorAll('img[alt]');
@@ -13,3 +13,22 @@ search.addEventListener('keyup', () => {
     }
   }
 });
+*/
+const input = document.getElementById('userInput');
+
+const search = () => {
+  const ul = document.getElementsByClassName('photos');
+  const li = ul.getElementsByTagName('li');
+  const filter = input.value.toUpperCase();
+
+  for (i = 0; i <li.length; i ++) {
+    let imageTitle = i.getAttribute('data-title');
+    if (imageTitle.indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+input.addEventListener('keyup', search);
